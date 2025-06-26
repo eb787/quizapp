7a921bf461f27204840cba9dba5b62ab9fda6d58
eb.developer87@gmail.com <eb.developer87@gmail.com>
2025-06-26 12:12:04 +0200

    code complete

---
+++ sakura17/sakura/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, MainContentComponent, FooterComponent, ImprintComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'sakura';
+}
